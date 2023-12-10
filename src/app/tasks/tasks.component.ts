8113284f0e704a2a516ae42294ecfc1ff8df0538
Jakub <kubaok96@gmail.com>
2023-12-11 00:33:27 +0100

    Add lab-e

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TasksService} from "../tasks.service";
 import {Task} from "../task";
 import {forkJoin, Observable} from "rxjs";
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -11,28 +12,35 @@ export class TasksComponent implements OnInit {
   public tasks: Task[] = [];
   public newTask: Task = {};
   public isProcessing = false;
+
   constructor(
     private tasksService: TasksService
   ) {
   }
+
   ngOnInit() {
     this.tasksService.index().subscribe((tasks) => {
       this.tasks = tasks;
     });
     this.isProcessing = false;
   }
+
   addTask() {
     if (this.newTask.title === undefined) {
       return;
     }
+
     this.newTask.completed = false;
     this.newTask.archived = false;
+
     this.tasks.unshift(this.newTask);
+
     this.tasksService.post(this.newTask).subscribe((task) => {
       this.newTask = {};
       this.ngOnInit();
     });
   }
+
   handleChange(task: Task) {
     this.tasksService.put(task).subscribe({
       error: err => {
@@ -41,17 +49,37 @@ export class TasksComponent implements OnInit {
       }
     });
   }
+
   archiveCompleted() {
     const observables: Observable<any>[] = [];
     for (const task of this.tasks) {
       if (!task.completed) {
         continue;
       }
+
       task.archived = true;
       observables.push(this.tasksService.put(task));
     }
+
     forkJoin(observables).subscribe(() => {
       this.ngOnInit();
     });
   }
+
+  canArchiveCompleted() {
+    for (const task of this.tasks) {
+      if (task.completed) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  canAddTask() {
+    if (this.isProcessing) {
+      return false;
+    }
+    return !!this.newTask.title;
+  }
+
 }
